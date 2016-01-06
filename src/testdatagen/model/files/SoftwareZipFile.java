44135eb30e7c1e5205220b9bb4f84d8e84c5d203
sermo-de-arboribus <sermo_de_arboribus@seznam.cz>
2016-01-06 17:36:05 +0100

    Generate Software-Zip files

--- src/testdatagen/model/files/SoftwareZipFile.java
+++ src/testdatagen/model/files/SoftwareZipFile.java
@@ -1,8 +1,16 @@
 package testdatagen.model.files;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.apache.commons.io.FilenameUtils;
 
 import testdatagen.model.Title;
+import testdatagen.utilities.Utilities;
 
 public class SoftwareZipFile extends EBookFile
 {
@@ -24,8 +32,36 @@ public class SoftwareZipFile extends EBookFile
 	@Override
 	public File generate(final File destDir) 
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String destPath = FilenameUtils.concat(destDir.getPath(), buildFileName());
+		Random random = new Random();
+		ZipOutputStream out = null;
+		
+		try
+		{
+			out = new ZipOutputStream(new FileOutputStream(destPath));
+			int numberOfIterations = random.nextInt(19) + 1;
+			for(int i = 1; i <= numberOfIterations; i++ )
+			{
+				ZipEntry nextFile = new ZipEntry("dummyfile_" + i + ".exe");
+				nextFile.setMethod(ZipOutputStream.DEFLATED);
+				out.putNextEntry(nextFile);
+				int fileSize = random.nextInt(300000) + 1;
+				byte[] randomBytes = new byte[fileSize];
+				random.nextBytes(randomBytes);
+				out.write(randomBytes);
+				out.closeEntry();
+			}
+			return new File(destPath);
+		}
+		catch(IOException exc)
+		{
+			Utilities.showErrorPane("Error zipping SoftwareZip file " + buildFileName(), exc);
+			return null;
+		}
+		finally
+		{
+			Utilities.safeClose(out);
+		}
 	}
 	
 	@Override
